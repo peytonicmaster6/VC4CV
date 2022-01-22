f3382ddb3954610844b02bd953d3f3ccac8648cc
peytonicmaster6 <80078168+peytonicmaster6@users.noreply.github.com>
2022-01-22 00:49:38 -0500

    Update vert.glsl

--- gl_shaders/CamES/vert.glsl
+++ gl_shaders/CamES/vert.glsl
@@ -20,29 +20,10 @@ vec2 Distort(vec2 p)
 
 void main()
 {
-    //vec3 v = vPos.xyz;
-
-    //float radius = length(vPos.xyz);
- 
-    //float theta = atan(vTex.y,vTex.x);
-
-    //radius = pow(radius, 1.0); //changes zoom again
-
-    //v.x = radius * cos(theta);
-    //v.y = radius * sin(theta);
-    
-    //gl_Position = vec4(Distort(vPos), 1.0);
-    //gl_Position = vec4(gl_Position.xyz, 1.0);
-    //vec3 pos = vPos;
-    //pos.y = 1.0*cos(1.0);
-    //pos.xy = vTex.xy;
-    //pos.xy = pos.xy*0.5 + 0.0;
-
-
-
     gl_Position = vec4(vPos, 1.0); //second value is zoom 
 
     uv = vTex;
+    //Apply barrel distortion
     uv = uv*2.0 - 1.0;
     uv = Distort(uv);
 }
